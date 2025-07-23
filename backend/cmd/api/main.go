da3aea4e88a40ab6ede9e5db9577c4b624c2531a
Brandon Tripp <brandongtripp@gmail.com>
2025-07-23 14:38:16 -0600

    fix(base_api): align postgres env var names in main.go

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -16,11 +16,11 @@ import (
 
 func main() {
 
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("POSTGRES_HOST")
+	dbPort := os.Getenv("POSTGRES_PORT")
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
 
 	connString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		dbHost, dbPort, dbUser, dbPassword, dbName)
