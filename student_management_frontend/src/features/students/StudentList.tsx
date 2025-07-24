0a1c02a173b7bb11034772b946c9a54a8dbfc58e
Brandon Tripp <brandongtripp@gmail.com>
2025-07-23 22:08:10 -0600

    feat(base_frontend): add a simple DaisyUI table to display the students

--- student_management_frontend/src/features/students/StudentList.tsx
+++ student_management_frontend/src/features/students/StudentList.tsx
@@ -1,8 +1,35 @@
-const StudentList = () => {
+import React from 'react';
+import type { Student } from '../../types'
+
+interface StudentListProps {
+  students: Student[]
+}
+
+const StudentList: React.FC<StudentListProps> = ({ students }) => {
   return (
-    <div className="p-4 bg-base-100 rounded-lg">Student list table will go here...</div>
+    <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student) => (
+            <tr key={student.id}>
+              <th>{student.id}</th>
+              <td>{student.name}</td>
+              <td>{student.grade}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
-export default StudentList
 
+
+export default StudentList
